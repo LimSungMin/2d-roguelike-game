4cd7d92608254eba7365b69c311f4352961474e6
sungmin <lsmorigin@gmail.com>
2018-02-11 01:37:31 +0900

    tutorial 기준으로 게임 완성
    - 유니티 공식 홈페이지 튜토리얼을 보고 게임 완성
    
    refs #1

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public static SoundManager instance = null;
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
+
+	// Use this for initialization
+	void Awake () {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        // scene 이 갱신되도 음악이 중지되지 않도록 설정
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void PlaySingle(AudioClip clip)
+    {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.pitch = randomPitch;
+        efxSource.clip = clips[randomIndex];
+        efxSource.Play();
+    }
+}
